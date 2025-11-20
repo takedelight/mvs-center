b6b160701bb82c2965260f3ba9ff31fd25ebbafb
Nikolaenko <aleshka.dev@gmail.com>
2025-11-20 13:35:35 +0200

    add root layout

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,13 +5,21 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ErrorBoundary } from 'react-error-boundary';
 import { App } from './App.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { RootLayout } from './layout.tsx';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
         <StrictMode>
             <ErrorBoundary fallback={<h1>Something went wrong</h1>}>
-                <App />
+                <BrowserRouter>
+                    <Routes>
+                        <Route element={<RootLayout />}>
+                            <Route index element={<App />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
             </ErrorBoundary>
         </StrictMode>
         <ReactQueryDevtools initialIsOpen={false} />
