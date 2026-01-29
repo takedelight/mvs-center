b0f4a262312a53ee71f8d13d2a02166f267db94c
Nikolaenko <aleshka.dev@gmail.com>
2026-01-29 20:47:22 +0200

    refactor AllStatementsPage to improve loading state handling and enhance no results display

--- src/app/admin/statements/page.tsx
+++ src/app/admin/statements/page.tsx
@@ -114,23 +114,26 @@ const AllStatementsPage = () => {
                 </TableRow>
               )}
 
-              {!isLoading && table.getRowModel().rows?.length
-                ? table.getRowModel().rows.map((row) => (
-                    <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-                      {row.getVisibleCells().map((cell) => (
-                        <TableCell key={cell.id}>
-                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))
-                : !isLoading && (
-                    <TableRow>
-                      <TableCell colSpan={columns.length} className="h-24 text-center">
-                        No results.
+              {!isLoading && table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
                       </TableCell>
-                    </TableRow>
-                  )}
+                    ))}
+                  </TableRow>
+                ))
+              ) : !isLoading ? (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-[540px] text-center align-middle text-muted-foreground"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              ) : null}
             </TableBody>
           </Table>
         </div>
